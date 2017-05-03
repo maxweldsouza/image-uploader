a3ae53d3d890e52e72feef5290e886c0696664e3
Maxwel Dsouza <maxellusionist@gmail.com>
2017-05-03 23:16:26 +0530

    Fix image display

--- Gallery.jsx
+++ Gallery.jsx
@@ -4,11 +4,15 @@ import { Image } from 'cloudinary-react';
 export default class Gallery extends React.Component {
     render() {
         return (<div>
-            <h1>Image Gallery</h1>
+            <h1>Uploaded Images</h1>
+            <h2>Vertical 365 x 450</h2>
+            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="365" height="450" crop="fill" gravity="auto" />
+            <h2>Horizontal 755 x 450</h2>
+            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="755" height="450" crop="fill" gravity="auto" />
+            <h2>Horizontal Small 365 x 212</h2>
+            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="365" height="212" crop="fill" gravity="auto" />
+            <h2>Gallery 380 x 380</h2>
             <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="380" height="380" crop="fill" gravity="auto" />
-            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="212" height="365" crop="fill" gravity="auto" />
-            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="450" height="365" crop="fill" gravity="auto" />
-            <Image cloudName="comparnion" publicId="Aldrin_Apollo_11" width="450" height="755" crop="fill" gravity="auto" />
         </div>);
     }
 }
