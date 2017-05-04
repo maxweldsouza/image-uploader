151f3a8d957a7968a522c8b6a7682ea104d52e87
Maxwel Dsouza <maxellusionist@gmail.com>
2017-05-04 14:50:57 +0530

    Fix gallery

--- Gallery.jsx
+++ Gallery.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Image } from 'cloudinary-react';
 
-export default function Gallery() {
+export default function Gallery({ publicId }) {
     return (
         <div>
             <h3>Vertical 365 x 450</h3>
             <Image
                 cloudName="comparnion"
-                publicId={this.props.publicId}
+                publicId={publicId}
                 width="365"
                 height="450"
                 crop="fill"
@@ -16,7 +16,7 @@ export default function Gallery() {
             <h3>Horizontal 755 x 450</h3>
             <Image
                 cloudName="comparnion"
-                publicId={this.props.publicId}
+                publicId={publicId}
                 width="755"
                 height="450"
                 crop="fill"
@@ -25,7 +25,7 @@ export default function Gallery() {
             <h3>Horizontal Small 365 x 212</h3>
             <Image
                 cloudName="comparnion"
-                publicId={this.props.publicId}
+                publicId={publicId}
                 width="365"
                 height="212"
                 crop="fill"
@@ -34,7 +34,7 @@ export default function Gallery() {
             <h3>Gallery 380 x 380</h3>
             <Image
                 cloudName="comparnion"
-                publicId={this.props.publicId}
+                publicId={publicId}
                 width="380"
                 height="380"
                 crop="fill"
